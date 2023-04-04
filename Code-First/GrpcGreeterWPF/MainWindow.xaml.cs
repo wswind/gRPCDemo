5db0583b2ab77555949cbff779cb777fac9f1bc5
Vincent Wang <ws_dev@163.com>
2023-04-04 15:55:26 +0800

    init

---
+++ Code-First/GrpcGreeterWPF/MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using Grpc.Net.Client;
+using ProtoBuf.Grpc.Client;
+using Shared.Contracts;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace GrpcGreeterWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var channel = GrpcChannel.ForAddress("https://localhost:7077");
+            var client = channel.CreateGrpcService<IGreeterService>();
+
+            var reply = client.SayHelloAsync(
+                new HelloRequest { Name = "GreeterClient" }).GetAwaiter().GetResult();
+
+            textbox1.Text = reply.Message;
+        }
+    }
+}
