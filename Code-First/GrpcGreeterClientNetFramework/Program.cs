6cd1cb86fb1bb2e215ffdfe4db0e0c7c60d80075
Vincent Wang <ws_dev@163.com>
2023-05-29 16:11:38 +0800

    net framework console

---
+++ Code-First/GrpcGreeterClientNetFramework/Program.cs
@@ -0,0 +1,29 @@
+﻿using Grpc.Core;
+using ProtoBuf.Grpc.Client;
+using Shared.Contracts;
+using System;
+using System.Threading.Tasks;
+
+namespace GrpcGreeterClientNetFramework
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var channel = new Channel("localhost", 5186, ChannelCredentials.Insecure);
+
+            try
+            {
+                var client = channel.CreateGrpcService<IGreeterService>();
+                var reply = await client.SayHelloAsync(
+                    new HelloRequest { Name = "GreeterClient" });
+
+                Console.WriteLine(reply.Message);
+            }
+            finally
+            {
+                channel.ShutdownAsync().GetAwaiter().GetResult();
+            }
+        }
+    }
+}
